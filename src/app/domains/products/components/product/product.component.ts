60a23026623b613d90a5236db2cd54189d66d00c
JesusChamiso <jesus.chamiso@hotmail.com>
2024-12-22 15:42:34 -0400

    Inputs y Outputs en Angular

---
+++ src/app/domains/products/components/product/product.component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  imports: [],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css',
+})
+export class ProductComponent {
+  @Input({required: true}) img = '';
+  @Input({required: true}) price = 0;
+  @Input({required: true}) title = '';
+
+  @Output() addToCart = new EventEmitter();
+  addTOCartHandler() {
+    console.log('click from Child');
+    this.addToCart.emit(
+      'Hola este es un mensaje desde el hijo '+ this.title
+    );
+  }
+
+}
