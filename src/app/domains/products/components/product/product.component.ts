cdaaf634330c17d0c025ff0329474103b248354e
JesusChamiso <jesus.chamiso@hotmail.com>
2024-12-23 00:13:50 -0400

    Galeria de Imagenes

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,11 +1,12 @@
 import { CurrencyPipe, UpperCasePipe } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { RouterLinkWithHref } from '@angular/router';
 import { Product } from '@shared/models/product.model';
 import { TimeAgoPipe } from '@shared/pipes/time-ago.pipe';
 
 @Component({
   selector: 'app-product',
-  imports: [UpperCasePipe, CurrencyPipe, TimeAgoPipe],
+  imports: [UpperCasePipe, CurrencyPipe, TimeAgoPipe, RouterLinkWithHref],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
--- src/app/domains/shared/services/product.service.ts
+++ src/app/domains/shared/services/product.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
-import { map } from 'rxjs';
+// import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +9,28 @@ import { map } from 'rxjs';
 export class ProductService {
 
   private http = inject(HttpClient);
-  private apiUrl = 'https://api.escuelajs.co/api/v1/products';
+  private apiUrlProducts = 'https://api.escuelajs.co/api/v1/products';
 
   getProducts() {
     // aqui se puede retirar el pipe() si el servicio de API tiene el campo createdAt, en este caso como no Lo tiene se agrega uno aleatorio
-    return this.http.get<Product[]>(this.apiUrl).pipe(
-      map((products) => 
-        products.map(
-          (product)=>({
-            ...product,
-            createdAt: product.createdAt || this.getRandomDate(new Date(2022, 0, 1), new Date(2024, 11,31))
-          })
-      ))
-    );
+    // return this.http.get<Product[]>(this.apiUrlProducts).pipe(
+    //   map((products) => 
+    //     products.map(
+    //       (product)=>({
+    //         ...product,
+    //         createdAt: product.createdAt || this.getRandomDate(new Date(2022, 0, 1), new Date(2024, 11,31))
+    //       })
+    //   ))
+    // );
+    return this.http.get<Product[]>(this.apiUrlProducts);
   }
 
   getRandomDate(start: Date,end: Date):string {
     const randomTime = new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
     return randomTime.toISOString();
+  }
+
+  getOne(id:number) {
+    return this.http.get<Product>(this.apiUrlProducts+`/${id}`);
   }
 }
